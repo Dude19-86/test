78e5997d524dacd4b3e6f2a85e0f1a870fd922b8
YDude <mrcrueloneman@gmail.com>
2022-04-19 17:18:35 +0300

    1

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict";
+// a = 20;
+
+// let f = 10;
+// let fuckhope = 20;
+// let leftButton = 100
+// console.log(f, fuckhope, leftButton);
+//
+// // const obj = {
+// //     a: 50
+// // };
+//
+// // obj.a = 10
+// // console.log(obj);
+// console.log(name)
+// var name = 'Ivan';
+//
+//
+// let result;
+// console.log(result)
+//
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+//
+// console.log(obj['name'])
+//
+// let arr = ['plum.png', 'orange.png', 6, {}, 'blue.png', []];
+// console.log(arr[4])
+
+
+// alert(true)
+
+// let result = confirm('Are you sure you want to')
+// let result1 = prompt('Are you sure you want to')
+// console.log(result1)
+
+// const category = 'toys';
+// console.log(`http://someurl.com/${category}/4`);
+//
+// const user = 'Ivan';
+//
+// alert(`hi, ${user}`)
+
